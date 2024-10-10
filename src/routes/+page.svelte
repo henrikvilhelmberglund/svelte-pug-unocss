5f9fa068adea2364da08f8980f90c20d6649ad8a
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-10-10 16:55:03 +0200

    feat: first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<template lang="pug">
+  div(class="page")
+  h1 Hello World
+  p(class="flex pt-4 text-center text-[red] text-3px") I'm a cool Vite project!
+  div(class="bg-blue-500 text-white text-40 text-ellipsis p-[16px]")
+    h1 Привет, мир!
+  div.bg-blue-500.text-white.p-4.m-2 PUG!!!!
+</template>
